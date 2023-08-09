dfef6b3c2f3f95788c791cb62a58298e607bb077
Reaceee <davecod9@gmail.com>
2023-08-09 18:57:02 +0100

    Just trying the label

--- src/components/FaqCard.jsx
+++ src/components/FaqCard.jsx
@@ -1,8 +1,8 @@
 const FAQ_card = (props) => {
     return (
         <>
-            <label htmlFor={'check' + props.id} className="faqCard h-max flex flex-col w-full px-5 pt-4 pb-2 ring-1 ring-slate-400 gap-2 shadow-md rounded-md">
-                <input type="checkbox" id={'check' + props.id} className="hidden invisible checkBox" />
+            <label htmlFor={'check' + props.id} for={'check' + props.id} className="faqCard h-max flex flex-col w-full px-5 pt-4 pb-2 ring-1 ring-slate-400 gap-2 shadow-md rounded-md">
+                <input checked type="checkbox" id={'check' + props.id} className="checkBox hidden" />
                 <div className="flex w-full justify-between items-center">
                     <p className="question font-bold max-w-[80%]">{props.quest}?</p>
 
